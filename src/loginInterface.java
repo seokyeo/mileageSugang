4c325d79803dd065197f9d085e65ccddf9e149f2
seokyeo <seokregi@gmail.com>
2022-11-02 00:04:46 +0900

    1101작성

---
+++ src/loginInterface.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class loginInterface extends JFrame {
+    public loginInterface() {
+
+        setTitle("마일리지 수강신청");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        GridLayout loginUiGrid = new GridLayout(3,2); 
+        loginUiGrid.setVgap(5);
+        loginUiGrid.setHgap(3);
+
+        Container loginUi = getContentPane();
+        loginUi.setLayout(loginUiGrid);
+        
+        loginUi.add(new JLabel("ID"));
+        loginUi.add(new JTextField(""));
+        loginUi.add(new JLabel("Password"));
+        loginUi.add(new JTextField(""));
+        JButton signUpButton = new JButton("회원가입");
+        signUpButton.addActionListener(signUpButtonAction);
+        loginUi.add(signUpButton);
+        JButton loginButton = new JButton("로그인");
+        loginButton.addActionListener(loginButtonAction);
+        loginUi.add(loginButton);
+
+        setSize(320, 180);
+        setVisible(true);
+
+    }
+    ActionListener signUpButtonAction = new ActionListener(){
+        public void actionPerformed(ActionEvent e) {
+            //signup();
+            dispose();
+    }
+    };
+    ActionListener loginButtonAction = new ActionListener(){
+        public void actionPerformed(ActionEvent e) {
+            new mainInterface();
+            dispose();
+    }
+    };
+}
